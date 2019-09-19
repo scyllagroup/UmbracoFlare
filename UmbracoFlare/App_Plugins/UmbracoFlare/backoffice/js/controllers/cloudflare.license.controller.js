2f405b2a036ac427dae0c1f7803f227c1d3ecaec
John Paul McAveney <jmcaveney@scyllagroup.com>
2019-09-19 16:55:32 -0400

    remove duplicate files

--- UmbracoFlare/App_Plugins/UmbracoFlare/backoffice/js/controllers/cloudflare.license.controller.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module("umbraco").controller("Cloudflare.License.Controller",
-	function ($scope, $timeout, cloudflareResource, notificationsService, navigationService) {
-
-
-	});
--- UmbracoFlare/App_Plugins/UmbracoFlare/backoffice/js/utilities/treeToggler.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('umbraco').controller('demo.toggler', function ($scope, appState, eventsService) {
-    //Create an Angular JS controller and inject Umbraco services appState & eventsService
-
-    
-
-});
